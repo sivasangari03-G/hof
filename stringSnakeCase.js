9c21a12a9c4228defe7ad088d1360937749a8382
SANGARIGURU <sangariguru03@gmail.com>
2022-01-13 17:58:16 +0530

    snake case using reduce

--- stringSnakeCase.js
+++ stringSnakeCase.js
@@ -1,9 +1,9 @@
 //Given an array of strings print the snake case of the strings (reduce) [Assume Small case strings]
 
-var arr = ["camelCase", "snake_case", "snack_case_example", "camelCaseExample"];
+var arr = ["a", "b", "c", "d"];
 
-var res = arr.filter(function (elem) {
-    return elem.includes("_")
+var res = arr.reduce(function (prevValue, currValue) {
+    return `${prevValue}_${currValue}`
 })
 
 console.log(res);
